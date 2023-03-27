77c6974d8470ff412a6598ef99950d2d88ac3220
theosth <67734722+theosth@users.noreply.github.com>
2023-03-27 16:26:30 +0200

    c

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ python /app/repo/train.py
 # Create output directory if it doesn't exist
 mkdir -p /app/repo/output
 
-# Move the output.txt to the output directory
-mv /app/repo/model /app/repo/output/model
+# Move the model to the output directory
+mv /app/repo/model /app/repo/output/
